679fe9f440e1e2f25fb02f1c54f48bacb53b5c9e
tescales <toscal@microsoft.com>
2019-09-25 14:48:57 -0700

    update using ACR

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -7,32 +7,29 @@ az login --identity
 az configure --defaults location=$AZURE_LOCATION
 az configure --defaults group=$AZURE_RESOURCE_GROUP
 
-az acr create --name $AZURE_RESOURCE_GROUP --sku Standard
-az acr build --image my/syncserver:v1 --registry $AZURE_RESOURCE_GROUP --file Dockerfile .
+cp /code/$GITHUB_REPO/Dockerfile /mozilla/syncserver/Dockerfile -f
+cd /mozilla/syncserver
 
-spID=$(az container show -n bootstrapper --query identity.principalId --out tsv)
-resourceID=$(az acr show --resource-group $AZURE_RESOURCE_GROUP --name $AZURE_RESOURCE_GROUP --query id --output tsv)
-#az role assignment create --assignee $spID --scope $resourceID --role acrpull
+#az acr create --name $AZURE_RESOURCE_GROUP --sku Standard --admin-enabled true
 
-#az acr build --registry $ACR_NAME --image ffoxsync:v1 .
+az acr build --image syncserver:v1 --registry $AZURE_RESOURCE_GROUP --file Dockerfile .
 
+acruser=$($AZURE_RESOURCE_GROUP)
+password=$(az acr credential show --name $AZURE_RESOURCE_GROUP --query passwords[0].value --out tsv)
+imageServer=$(az acr show --resource-group ffsync --name ffsync --query loginServer --output tsv)
 
 echo Setting up storage accounts...
 az storage share create -n $AZURE_STORAGE_SHARE --account-name $AZURE_STORAGE_ACCOUNT --account-key $AZURE_STORAGE_KEY
 az storage share policy create -n $AZURE_STORAGE_ACCOUNT -s $AZURE_STORAGE_SHARE --permissions dlrw
 
-echo adding Firefox user
-addgroup -g 1001 app && adduser -u 1001 -S -D -G app -s /usr/sbin/nologin app
-usermod -aG sudo app
-
-echo Copying your configuration...
-cp /code/syncserver.ini /syncserver/syncserver.ini -f
-
-echo Building Firefox Sync Server...
-cd /syncserver
-make build
-make test
-make serve
+echo Deploying Firefox syncserver container...
+az container create --resource-group $AZURE_RESOURCE_GROUP --name $AZURE_RESOURCE_GROUP\
+ --image $imageServer/syncserver:v1 --registry-username=$acruser --registry-password=$password\
+ --azure-file-volume-mount-path "/home/$AZURE_STORAGE_SHARE" --azure-file-volume-account-key $AZURE_STORAGE_KEY\
+ --azure-file-volume-account-name $AZURE_STORAGE_ACCOUNT --azure-file-volume-share-name $AZURE_STORAGE_SHARE\
+ --ip-address Public --dns-name-label $AZURE_STORAGE_ACCOUNT  --ports 5000\
+ --environment-variables SYNCSERVER_SQLURI="sqlite:////home/ffsync/syncserver.db" FF_SYNCSERVER_PUBLIC_URL="$($AZURE_RESOURCE_GROUP).$($AZURE_LOCATION).azurecontainer.io" 
+ 
 
 ## uncomment the below statement to troubleshoot your startup script interactively in ACI (on the Connect tab)
 tail -f /dev/null
