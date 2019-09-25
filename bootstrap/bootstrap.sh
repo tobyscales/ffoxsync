cfaacbce4ce11b3a3ece78ea922a7f80b4765394
tescales <toscal@microsoft.com>
2019-09-25 11:28:34 -0700

    try native

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -10,5 +10,12 @@ az configure --defaults group=$AZURE_RESOURCE_GROUP
 az storage share create -n $AZURE_STORAGE_SHARE --account-name $AZURE_STORAGE_ACCOUNT --account-key $AZURE_STORAGE_KEY
 az storage share policy create -n $AZURE_STORAGE_ACCOUNT -s $AZURE_STORAGE_SHARE --permissions dlrw
 
+cp /code/syncserver.ini /syncserver/syncserver.ini -f
+
+cd /syncserver
+make build
+make test
+make serve
+
 ## uncomment the below statement to troubleshoot your startup script interactively in ACI (on the Connect tab)
 tail -f /dev/null
