175bdf6e5aa9d7885e30dd422c607b21ac861db5
tescales <toscal@microsoft.com>
2019-09-25 12:16:52 -0700

    build Dockerfile at runtime

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -7,11 +7,18 @@ az login --identity
 az configure --defaults location=$AZURE_LOCATION
 az configure --defaults group=$AZURE_RESOURCE_GROUP
 
+echo Setting up storage accounts...
 az storage share create -n $AZURE_STORAGE_SHARE --account-name $AZURE_STORAGE_ACCOUNT --account-key $AZURE_STORAGE_KEY
 az storage share policy create -n $AZURE_STORAGE_ACCOUNT -s $AZURE_STORAGE_SHARE --permissions dlrw
 
+echo adding Firefox user
+addgroup -g 1001 app && adduser -u 1001 -S -D -G app -s /usr/sbin/nologin app
+usermod -aG sudo app
+
+echo Copying your configuration...
 cp /code/syncserver.ini /syncserver/syncserver.ini -f
 
+echo Building Firefox Sync Server...
 cd /syncserver
 make build
 make test
