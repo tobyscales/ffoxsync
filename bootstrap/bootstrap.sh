af7ffb370a65450cbd38f9271d0ea471376b773c
Toby Scales <toscal@microsoft.com>
2020-03-04 16:35:54 -0800

    update bootstrap.sh + add syncserver.ini

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -5,7 +5,7 @@ echo Location: $AZURE_LOCATION
 echo Resource Group: $AZURE_RESOURCE_GROUP
 
 # cribbed from http://fahdshariff.blogspot.com/2014/02/retrying-commands-in-shell-scripts.html
-# Retries a command on failure.
+# Retries a command on failure. 
 # $1 - the max number of attempts
 # $2... - the command to run
 
@@ -45,6 +45,9 @@ echo Setting up Nginx storage accounts...
 az storage share create -n nginx-config --account-name $AZURE_STORAGE_ACCOUNT --account-key $AZURE_STORAGE_KEY
 az storage share policy create -n $AZURE_STORAGE_ACCOUNT -s nginx-config --permissions dlrw
 
+az storage share create -n ff-config --account-name $AZURE_STORAGE_ACCOUNT --account-key $AZURE_STORAGE_KEY
+az storage share policy create -n $AZURE_STORAGE_ACCOUNT -s ff-config --permissions dlrw
+
 az storage share create -n nginx-html --account-name $AZURE_STORAGE_ACCOUNT --account-key $AZURE_STORAGE_KEY
 az storage share policy create -n $AZURE_STORAGE_ACCOUNT -s nginx-html --permissions dlrw
 
@@ -52,7 +55,10 @@ az storage share create -n nginx-certs --account-name $AZURE_STORAGE_ACCOUNT --a
 az storage share policy create -n $AZURE_STORAGE_ACCOUNT -s nginx-certs --permissions dlrw
 
 az storage file upload --source /$BOOTSTRAP_REPO/conf/default.conf --share-name nginx-config 
+az storage file upload --source /$BOOTSTRAP_REPO/conf/syncserver.ini --share-name ff-config
 az storage file upload --source /$BOOTSTRAP_REPO/html/index.html --share-name nginx-html 
+
+#"set" | az container exec --exec-command /bin/sh -n $AZURE_RESOURCE_GROUP -g $AZURE_RESOURCE_GROUP 
 
 ## uncomment the below statement to troubleshoot your startup script interactively in ACI (on the Connect tab)
 #tail -f /dev/null
